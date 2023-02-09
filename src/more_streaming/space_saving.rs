0432e898fbe0239149d689537b96e12b57d31899
roytechnion <123637639+roytechnion@users.noreply.github.com>
2023-02-09 17:44:28 +0200

    Improve parameters namings + use only constants in tests + better documentation

--- src/more_streaming/space_saving.rs
+++ src/more_streaming/space_saving.rs
@@ -26,7 +26,7 @@ pub struct SpaceSaving<K: Hash + std::cmp::Eq, V: std::cmp::Ord> {
     counters: DoublePriorityQueue<K, V>,
     capacity: usize,
     num: usize,
-    rap: bool,
+    rap: bool, // indicates whether we execute the RAP optimization
 }
 
 impl <K, V>SpaceSaving<K,V> 
@@ -46,6 +46,7 @@ V: std::cmp::Ord + std::ops::Add<Output=V> + std::ops::AddAssign + TryFrom<u8> +
         }
     }
 
+    /// insert an item into space saving (or add its counter if exists)
     pub fn insert(&mut self, id: K) 
     where <V as TryFrom<u8>>::Error: Debug
     {
@@ -66,6 +67,7 @@ V: std::cmp::Ord + std::ops::Add<Output=V> + std::ops::AddAssign + TryFrom<u8> +
         }
     }
 
+    /// return an estimate of an item's value from space saving
     pub fn get(&self, id: K) -> V {
         if let Some(val) = self.counters.get_priority(&id) {
             return *val;
@@ -75,6 +77,7 @@ V: std::cmp::Ord + std::ops::Add<Output=V> + std::ops::AddAssign + TryFrom<u8> +
 
     }
 
+    /// returns the number of entries in space saving
     pub fn capacity(&self) -> usize {
         return self.capacity;
     }
@@ -87,20 +90,24 @@ V: std::cmp::Ord + std::ops::Add<Output=V> + std::ops::AddAssign + TryFrom<u8> +
 
 #[cfg(test)]
 mod tests {
+    const TEST_ERROR: f64 = 0.01;
+    const TEST_N_ITEMS: usize = 30_000;
+    const TEST_ERROR_TOLERANCE: usize = 400;
+
     #[test]
     fn test_increment() {
-		let mut ss:super::SpaceSaving<&str,u32> = super::SpaceSaving::new(0.01,false);
-		for _ in 0..30_000 {
-			let _ = ss.insert("key");
+		let mut spacesaving:super::SpaceSaving<&str,u32> = super::SpaceSaving::new(TEST_ERROR,false);
+		for _ in 0..TEST_N_ITEMS {
+			let _ = spacesaving.insert("key");
 		}
-		assert!(30_000u32.abs_diff(ss.get("key")) < 400, "DIFF nh = {}", ss.get("key"));
+		assert!(TEST_N_ITEMS.abs_diff(usize::try_from(spacesaving.get("key")).unwrap()) < TEST_ERROR_TOLERANCE, "DIFF spacesaving = {}", spacesaving.get("key"));
 	}
     #[test]
     fn test_rap_increment() {
-		let mut ss:super::SpaceSaving<&str,u32> = super::SpaceSaving::new(0.01,true);
-		for _ in 0..30_000 {
-			let _ = ss.insert("key");
+		let mut spacesaving:super::SpaceSaving<&str,u32> = super::SpaceSaving::new(TEST_ERROR,true);
+		for _ in 0..TEST_N_ITEMS {
+			let _ = spacesaving.insert("key");
 		}
-		assert!(30000u32.abs_diff(ss.get("key")) < 400, "DIFF ss = {}", ss.get("key"));
+		assert!(TEST_N_ITEMS.abs_diff(usize::try_from(spacesaving.get("key")).unwrap()) < TEST_ERROR_TOLERANCE, "DIFF spacesaving = {}", spacesaving.get("key"));
 	}
 }
