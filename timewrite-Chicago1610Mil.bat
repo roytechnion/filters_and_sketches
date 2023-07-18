c536a482ecb6edd9018d785e8fcecc0313162692
roytechnion <123637639+roytechnion@users.noreply.github.com>
2023-07-18 12:48:07 +0300

    timing results

---
+++ timewrite-Chicago1610Mil.bat
@@ -0,0 +1,109 @@
+@ECHO OFF
+
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type HASH >> hash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type HASH >> hash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type HASH >> hash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type HASH >> hash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type HASH >> hash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type HASH >> hash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type HASH >> hash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type HASH >> hash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type HASH >> hash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type HASH >> hash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type HASH >> hash-write.txt
+
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroHash >> nitrohash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroHash >> nitrohash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroHash >> nitrohash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroHash >> nitrohash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroHash >> nitrohash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroHash >> nitrohash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroHash >> nitrohash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroHash >> nitrohash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroHash >> nitrohash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroHash >> nitrohash-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroHash >> nitrohash-write.txt
+
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type CMS >> CMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type CMS >> CMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type CMS >> CMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type CMS >> CMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type CMS >> CMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type CMS >> CMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type CMS >> CMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type CMS >> CMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type CMS >> CMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type CMS >> CMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type CMS >> CMS-write.txt
+
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCMS >> NitroCMS-write.txt
+
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type Cuckoo >> Cuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type Cuckoo >> Cuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type Cuckoo >> Cuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type Cuckoo >> Cuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type Cuckoo >> Cuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type Cuckoo >> Cuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type Cuckoo >> Cuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type Cuckoo >> Cuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type Cuckoo >> Cuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type Cuckoo >> Cuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type Cuckoo >> Cuckoo-write.txt
+
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCuckoo >> NitroCuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCuckoo >> NitroCuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCuckoo >> NitroCuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCuckoo >> NitroCuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCuckoo >> NitroCuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCuckoo >> NitroCuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCuckoo >> NitroCuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCuckoo >> NitroCuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCuckoo >> NitroCuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCuckoo >> NitroCuckoo-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type NitroCuckoo >> NitroCuckoo-write.txt
+
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving >> SpaceSaving-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving >> SpaceSaving-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving >> SpaceSaving-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving >> SpaceSaving-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving >> SpaceSaving-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving >> SpaceSaving-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving >> SpaceSaving-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving >> SpaceSaving-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving >> SpaceSaving-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving >> SpaceSaving-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving >> SpaceSaving-write.txt
+
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving --rap >> SpaceSaving-rap-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving --rap >> SpaceSaving-rap-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving --rap >> SpaceSaving-rap-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving --rap >> SpaceSaving-rap-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving --rap >> SpaceSaving-rap-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving --rap >> SpaceSaving-rap-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving --rap >> SpaceSaving-rap-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving --rap >> SpaceSaving-rap-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving --rap >> SpaceSaving-rap-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving --rap >> SpaceSaving-rap-write.txt
+cargo run --release -- --file-path "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" --ds-type SpaceSaving --rap >> SpaceSaving-rap-write.txt
---
+++ timewrite-all.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+REM timewrite-Chicago16Small
+REM timewrite-Chicago1610Mil
+REM timewrite-Chicago16
+REM timewrite
+REM timewrite-ny19A
+timewrite-ny19B
+timewrite-SJ14.small
+
