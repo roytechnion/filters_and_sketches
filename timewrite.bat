051ec5665c2ea749935665c094bc0271ef81cbf7
roytechnion <123637639+roytechnion@users.noreply.github.com>
2023-07-30 10:48:00 +0300

    Improved batch and new write measurements

--- timewrite.bat
+++ timewrite.bat
@@ -1,31 +1,31 @@
 @ECHO OFF
 
-FOR %%H IN (hash-readwrite.txt nitrohash-readwrite.txt CMS-readwrite.txt NitroCMS-readwrite.txt Cuckoo-readwrite.txt NitroCuckoo-readwrite.txt SpaceSaving-readwrite.txt SpaceSaving-rap-readwrite.txt) do ECHO "" > %%H
+FOR %%H IN (hash-write.txt nitrohash-write.txt CMS-write.txt NitroCMS-write.txt Cuckoo-write.txt NitroCuckoo-write.txt SpaceSaving-write.txt SpaceSaving-rap-write.txt) do ECHO "" > %%H
 
 FOR %%F IN ("c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago15.small.txt" "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago16Small.txt" "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago1610Mil.txt" "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\Chicago16.txt" "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\ny19A.txt" "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\ny19B.txt" "c:\users\user\Dropbox (Technion Dropbox)\traces\dataCounters\SJ14.small.txt") DO (
 	FOR /L %%G IN (1,1,11) DO (
-		cargo run --release -- --file-path %%F --ds-type HASH --time-type RWTIME >> hash-readwrite.txt
+		cargo run --release -- --file-path %%F --ds-type HASH --time-type WRITETIME >> hash-write.txt
 	)
 	FOR /L %%G IN (1,1,11) DO (
-		cargo run --release -- --file-path %%F --ds-type NitroHash --time-type RWTIME >> nitrohash-readwrite.txt
+		cargo run --release -- --file-path %%F --ds-type NitroHash --time-type WRITETIME >> nitrohash-write.txt
 	)
 	FOR /L %%G IN (1,1,11) DO (
-		cargo run --release -- --file-path %%F --ds-type CMS --time-type RWTIME >> CMS-readwrite.txt
+		cargo run --release -- --file-path %%F --ds-type CMS --time-type WRITETIME >> CMS-write.txt
 	)
 	FOR /L %%G IN (1,1,11) DO (
-		cargo run --release -- --file-path %%F --ds-type NitroCMS --time-type RWTIME >> NitroCMS-readwrite.txt
+		cargo run --release -- --file-path %%F --ds-type NitroCMS --time-type WRITETIME >> NitroCMS-write.txt
 	)
 	FOR /L %%G IN (1,1,11) DO (
-		cargo run --release -- --file-path %%F --ds-type Cuckoo --time-type RWTIME >> Cuckoo-readwrite.txt
+		cargo run --release -- --file-path %%F --ds-type Cuckoo --time-type WRITETIME >> Cuckoo-write.txt
 	)
 	FOR /L %%G IN (1,1,11) DO (
-		cargo run --release -- --file-path %%F --ds-type NitroCuckoo --time-type RWTIME >> NitroCuckoo-readwrite.txt
+		cargo run --release -- --file-path %%F --ds-type NitroCuckoo --time-type WRITETIME >> NitroCuckoo-write.txt
 	)
 	FOR /L %%G IN (1,1,11) DO (
-		cargo run --release -- --file-path %%F --ds-type SpaceSaving --time-type RWTIME >> SpaceSaving-readwrite.txt
+		cargo run --release -- --file-path %%F --ds-type SpaceSaving --time-type WRITETIME >> SpaceSaving-write.txt
 	)
 	FOR /L %%G IN (1,1,11) DO (
-		cargo run --release -- --file-path %%F --ds-type SpaceSaving --rap --time-type RWTIME >> SpaceSaving-rap-readwrite.txt
+		cargo run --release -- --file-path %%F --ds-type SpaceSaving --rap --time-type WRITETIME >> SpaceSaving-rap-write.txt
 	)
 )
 
